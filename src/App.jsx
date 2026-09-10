fa837b4c2295e16e10449d5c4ef92fcdc72d75e4
Mohamed Abdellah <mohamedabdellahalsayed@example.com>
2026-09-10 12:57:35 +0300

    Add loading state while fetching movies

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MovieCard  from './components/MovieCard'
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
   fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
     .then(response => response.json())
@@ -13,31 +14,44 @@ function App() {
       setMovies(data.results)
     })
 }, [])
-useEffect(()=>{
-  const timer = setTimeout(()=>{
-    if(searchTerm){
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setIsLoading(true)
+
+    if (searchTerm) {
       fetch(`https://api.themoviedb.org/3/search/movie?query=${searchTerm}&api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
-      .then(response => response.json())
-      .then(data => setMovies(data.results))
+        .then(response => response.json())
+        .then(data => {
+          setMovies(data.results)
+          setIsLoading(false)
+        })
     } else {
-  fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
-    .then(response => response.json())
-    .then(data => setMovies(data.results))
-}
+      fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+          setMovies(data.results)
+          setIsLoading(false)
+        })
+    }
   }, 500)
+
   return () => clearTimeout(timer)
-},[searchTerm])
+}, [searchTerm])
 return (
   <div className="min-h-screen bg-gray-900 flex flex-col items-center gap-6 py-10">
     <h1 className="text-4xl font-bold text-white">
       Movie Explorer 🎬
     </h1>
     <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-    <div className="flex flex-wrap gap-4 justify-center max-w-6xl">
-      {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+{isLoading ? (
+  <p className="text-white text-lg">Loading...</p>
+) : (
+  <div className="flex flex-wrap gap-4 justify-center max-w-6xl">
+    {movies.map(movie => (
+      <MovieCard key={movie.id} movie={movie} />
+    ))}
+  </div>
+)}
   </div>
 )
 }
